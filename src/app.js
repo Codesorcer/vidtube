218222af851eadaca719555b185cab0199ab4a8c
Codesorcer <Rudraprataps7789@gmail.com>
2024-12-14 16:42:59 +0530

    users routes and controllers complete

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from "cors"
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -14,5 +15,17 @@ app.use(
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded(({extended: true, limit: "16kb"})))
 app.use(express.static("public"))
+app.use(cookieParser())
+
+// import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import userRouter from "./routes/user.routes.js"
+
+
+// routes
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+
+
 
 export {app}
